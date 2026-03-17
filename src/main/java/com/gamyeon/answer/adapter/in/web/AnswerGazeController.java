2befd398d0343bc512d30604941dbc37ee7113ff
JaeWoong Eom <zzang6535@gmail.com>
2026-03-18 02:19:56 +0900

    Fix/typo (#70)
    * fix: application.yml typo 수정
    
    * fix(answer) : stt 요청 시 questionContent 필드 추가 (#62)
    
    * Fix/answer (#64)
    
    * fix(answer) : stt 요청 시 questionContent 필드 추가
    
    * fix(question) : base-url 호출 변경
    
    * Fix/answer (#65)
    
    * fix(answer) : stt 요청 시 questionContent 필드 추가
    
    * fix(question) : base-url 호출 변경
    
    * fix(answer): spotless
    
    * fix(answer) : spotless 최신화
    
    * fix: PythonQuestionGenerationFeignClient 수정
    
    - $ 추가
    
    * fix: PythonQuestionGenerationFeignClient 수정
    
    - 들여쓰기 수정
    
    * fix: PythonQuestionGenerationFeignClient 수정
    
    - 들여쓰기 수정
    
    * fix: Gaze Request 확인 위한 로그 추가
    
    * fix: Gaze Request 확인 위한 로그 추가
    
    - spotless
    
    ---------
    
    Co-authored-by: Hojun <milky9925@gmail.com>

--- src/main/java/com/gamyeon/answer/adapter/in/web/AnswerGazeController.java
+++ src/main/java/com/gamyeon/answer/adapter/in/web/AnswerGazeController.java
@@ -5,12 +5,14 @@ import com.gamyeon.answer.application.port.in.SendAnswerGazeSegmentUseCase;
 import com.gamyeon.answer.domain.AnswerSuccessCode;
 import com.gamyeon.common.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AnswerGazeController {
@@ -20,6 +22,7 @@ public class AnswerGazeController {
   @PostMapping("/api/v1/intvs/{questionSetId}/gaze")
   public ResponseEntity<ApiResponse<Void>> sendGazeSegment(
       Long userId, @PathVariable Long questionSetId, @RequestBody JsonNode requestBody) {
+    log.info("body: {}", requestBody.toString());
     userId = 1L;
     sendAnswerGazeSegmentUseCase.send(
         new com.gamyeon.answer.application.port.in.SendAnswerGazeSegmentCommand(
