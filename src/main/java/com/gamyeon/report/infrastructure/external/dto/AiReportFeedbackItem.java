7c069105abe32a9b9b43e819d7489f667af33ac7
JaeWoong Eom <zzang6535@gmail.com>
2026-03-19 01:27:46 +0900

    fix: AiReportFeedbackItem 수정 (#79)
    - JsonProperty 추가

--- src/main/java/com/gamyeon/report/infrastructure/external/dto/AiReportFeedbackItem.java
+++ src/main/java/com/gamyeon/report/infrastructure/external/dto/AiReportFeedbackItem.java
@@ -1,5 +1,6 @@
 package com.gamyeon.report.infrastructure.external.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,19 +13,38 @@ public class AiReportFeedbackItem {
   private String status;
 
   private Integer reliability;
+
+  @JsonProperty("logic_score")
   private Integer logicScore;
+
+  @JsonProperty("answer_composition_score")
   private Integer answerCompositionScore;
+
+  @JsonProperty("gaze_score")
   private Integer gazeScore;
+
+  @JsonProperty("time_score")
   private Integer timeScore;
+
+  @JsonProperty("answer_duration_ms")
   private Integer answerDurationMs;
+
+  @JsonProperty("keyword_count")
   private Integer keywordCount;
 
   private Integer index;
+
+  @JsonProperty("question_content")
   private String questionContent;
+
   private String characteristic;
+
+  @JsonProperty("answer_summary")
   private String answerSummary;
+
   private String strength;
   private String improvement;
 
+  @JsonProperty("feedback_badges")
   private List<String> feedbackBadges;
 }
