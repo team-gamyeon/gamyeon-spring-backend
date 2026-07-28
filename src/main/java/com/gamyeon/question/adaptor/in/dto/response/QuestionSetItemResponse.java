4d8dd0db27242a35ad65b83057c346cad5e4e6b7
Jung Hojun <milky9925@gmail.com>
2026-07-28 19:52:22 +0900

    refactor(questions): 질문 조회 api 호출 시 응답값에 qeustionOrder추가

--- src/main/java/com/gamyeon/question/adaptor/in/dto/response/QuestionSetItemResponse.java
+++ src/main/java/com/gamyeon/question/adaptor/in/dto/response/QuestionSetItemResponse.java
@@ -2,8 +2,8 @@ package com.gamyeon.question.adaptor.in.dto.response;
 
 import com.gamyeon.question.application.port.in.QuestionSetItemResult;
 
-public record QuestionSetItemResponse(Long questionSetId, String content) {
+public record QuestionSetItemResponse(Long questionSetId, String content, Integer questionOrder) {
   public static QuestionSetItemResponse from(QuestionSetItemResult result) {
-    return new QuestionSetItemResponse(result.id(), result.content());
+    return new QuestionSetItemResponse(result.id(), result.content(), result.questionOrder());
   }
 }
